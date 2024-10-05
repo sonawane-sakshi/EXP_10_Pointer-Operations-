20c341e5dae06fd2ed397cac07558d7a9fa27df0
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-05 21:18:07 +0530

    Add files via upload

---
+++ Swap_Value_Pointer.cpp
@@ -0,0 +1,13 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include <iostream>
+using namespace std;
+    int main(){
+        int a=10,b=20;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        cout<<"a = "<<a<<endl;
+        cout<<"b = "<<b<<endl;
+    }
